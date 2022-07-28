12e06c15ec495584479619f97cc48091152bf304
rakibul-cse-diu <rakibul2180@gmail.com>
2022-07-28 16:22:29 +0600

    initialization

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          "primary": "#FBD062",
+          "secondary": "#111430",
+          "accent": "#2D2D2D",
+          "neutral": "#707070",
+          "base-100": "#FFFFFF",
+          "info": "#3ABFF8",
+          "success": "#36D399",
+          "warning": "#FBBD23",
+          "error": "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+}
